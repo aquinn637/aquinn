e1b3d35cdf5de7898ba1c9fdada090f992bc9ed3
Aubrey Quinn <aquinn@softworks.com>
2020-09-28 12:42:45 +0100

    demo application

--- tools/demo/inappbrowser/index.ts
+++ tools/demo/inappbrowser/index.ts
@@ -1,8 +1,28 @@
 import { DemoSharedBase } from '../utils';
-import {} from '@aquinn/inappbrowser';
+import InAppBrowser from '@aquinn/inappbrowser';
+import { Utils } from '@nativescript/core';
 
 export class DemoSharedInappbrowser extends DemoSharedBase {
-	testIt() {
-		console.log('test inappbrowser!');
-	}
+
+    testIt() {
+        console.log('test inappbrowser!');
+
+        this.openLink("https://www.google.com");
+    }
+
+    async openLink(url) {
+        try {
+            if (InAppBrowser.isAvailable()) {
+                InAppBrowser.open(url);
+            }
+        }
+        catch (error) {
+            alert({
+                title: 'Error',
+                message: error.message,
+                okButtonText: 'Ok'
+            });
+        }
+    }
+
 }
