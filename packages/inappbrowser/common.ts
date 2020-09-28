8d73844644c3c12d4fe00fba99e110843b14f627
Aubrey Quinn <aquinn@softworks.com>
2020-09-28 12:31:16 +0100

    added code from other repository

--- packages/inappbrowser/common.ts
+++ packages/inappbrowser/common.ts
@@ -1,3 +1,33 @@
-import { Observable } from '@nativescript/core';
+export enum BROWSER_TYPES {
+    CANCEL = 'cancel',
+    DISMISS = 'dismiss',
+    SUCCESS = 'success'
+}
 
-export class InappbrowserCommon extends Observable {}
+export type RedirectEvent = {
+    url: 'string'
+};
+
+export type BrowserResult = {
+    type: 'cancel' | 'dismiss'
+    message?: string
+};
+
+export type RedirectResult = {
+    type: 'success',
+    url: string
+};
+
+export type AuthSessionResult = RedirectResult | BrowserResult;
+
+export function getDefaultOptions(url: string, options: any) {
+    return {
+        ...options,
+        url,
+        dismissButtonStyle: options.dismissButtonStyle || 'close',
+        readerMode: !!options.readerMode,
+        animated: options.animated !== undefined ? options.animated : true,
+        modalEnabled: options.modalEnabled !== undefined ? options.modalEnabled : true,
+        enableBarCollapsing: !!options.enableBarCollapsing
+    };
+}
